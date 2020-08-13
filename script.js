6bb5b1c5598079cf47a23a3c025d3f211d949c08
Vivid Green <zacharycreek@gmail.com>
2020-08-12 21:11:34 -0600

    Added home page and quiz-questions page along with some styling. Added object of quotes to script.js.

---
+++ script.js
@@ -0,0 +1,72 @@
+$( document ).ready(function() {
+    
+    const quotes = {
+        gandolf: {
+            quote: "YOU SHALL NOT PASS!",
+            imgSrc: "./Assets/img/gandolf.jpg"
+        },
+        leonardoDaVinci: {
+            quote: "Simplicity is the ultimate sophistication.",
+            imgSrc: "./Assets/img/leonardoDaVinci.jpg"
+        },
+        jfk: {
+            quote: "If not us, who? If not now, when?",
+            imgSrc: "./Assets/img/jfk.jpg"
+        },
+        gandhi: {
+            quote: "An eye for eye only ends up making the whole world blind.",
+            imgSrc: "./Assets/img/gandhi.jpg"
+        },
+        uncleIroh: {
+            quote: "While it is always best to believe in oneself, a little help from others can be a great blessing.",
+            imgSrc: "./Assets/img/uncleIroh.jpg"
+        },
+        nelsonMandela: {
+            quote: "Education is the most powerful weapon which you can use to change the world.",
+            imgSrc: "./Assets/img/nelsonMandela.jpg"
+        },
+        pippin: {
+            quote: "We've had one, yes. What about second breakfast?",
+            imgSrc: "./Assets/img/pippin.jpg"
+        },
+        shakespeare: {
+            quote: "Now is the winter of our discontent.",
+            imgSrc: "./Assets/img/shakespeare.jpg"
+        },
+        bellaAbzug: {
+            quote: "The test for whether or not you can hold a job should not be the arrangement of your chromosomes.",
+            imgSrc: "./Assets/img/bellaAbzug.jpg"
+        },
+        mlk: {
+            quote: "Darkness cannot drive out darkness; only light can do that. Hate cannot drive out hate; only love can do that.",
+            imgSrc: "./Assets/img/mlk.jpg"
+        },
+        v: {
+            quote: "Beneath this mask there is an idea. And ideas are bulletproof.",
+            imgSrc: "./Assets/img/v.jpg"
+        },
+        plato: {
+            quote: "The greatest wealth is to live content with little.",
+            imgSrc: "./Assets/img/plato.jpg"
+        },
+        fdr: {
+            quote: "The only thing we have to fear is fear itself.",
+            imgSrc: "./Assets/img/fdr.jpg"
+        },
+        tylerDurden: {
+            quote: "The things you own end up owning you.",
+            imgSrc: "./Assets/img/tylerDurden.jpg"
+        },
+        margaretThatcher: {
+            quote: "If you want something said, ask a man; if you want something done, ask a woman.",
+            imgSrc: "./Assets/img/margaretThatcher.jpg"
+        },
+        socrates: {
+            quote: "One thing only I know, and that is that I know nothing.",
+            imgSrc: "./Assets/img/socrates.jpg"
+        },
+    }
+    const quotesKeys = Object.keys(quotes);
+    console.log(quotes);
+    console.log(quotesKeys);
+});
